9a6a19df866a7b0e04aacaded0f9f0c246c3b483
dion-george <diongeorge@gmail.com>
2022-02-25 20:45:19 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import logo from './assets/img/couple-illustration.png'; // Tell webpack this JS file uses this image
+
+const Nav = styled.div`
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+`;
+
+const HeroComponent = styled.header`
+  background-color: #E16F7F;
+  display: flex;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <HeroComponent>
+      <Nav>
+        <p>social dimension</p>
+      </Nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="title-bold">
+          share the real you with your potential partner.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
+      <img src={logo} alt="Logo" />
+    </HeroComponent>
   );
 }
 
